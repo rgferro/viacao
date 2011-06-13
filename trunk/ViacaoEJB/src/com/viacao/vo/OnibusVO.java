6e8dc1ffe8444c9af16c4fac2223e6e793d71759
wallacecosme@gmail.com <wallacecosme@gmail.com@f410aa4f-024f-7bfd-2858-2e2917c05c61>
2011-06-13 18:32:04 +0000

    Wallace Gonçalves

--- trunk/ViacaoEJB/src/com/viacao/vo/OnibusVO.java
+++ trunk/ViacaoEJB/src/com/viacao/vo/OnibusVO.java
@@ -8,43 +8,75 @@ public class OnibusVO {
 	private String placa;
 	private Integer qtdPoltrona;
 	
+	/**
+	 * Obtém o atributo seqOnibus
+	 * @return Valor do atributo seqOnibus
+	 */
 	public Integer getSeqOnibus() {
 		return seqOnibus;
 	}
-	
+	/**
+	 * Altera o atributo seqOnibus
+	 * @param seqOnibus Novo valor do atributo
+	 */
 	public void setSeqOnibus(Integer seqOnibus) {
 		this.seqOnibus = seqOnibus;
 	}
-	
+	/**
+	 * Obtém o atributo tipo
+	 * @return Valor do atributo tipo
+	 */
 	public String getTipo() {
 		return tipo;
 	}
-	
+	/**
+	 * Altera o atributo tipo
+	 * @param tipo Novo valor do atributo
+	 */
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
+	/**
+	 * Obtém o atributo empresa
+	 * @return Valor do atributo empresa
+	 */
 	public String getEmpresa() {
 		return empresa;
 	}
-	
+	/**
+	 * Altera o atributo empresa
+	 * @param empresa Novo valor do atributo
+	 */
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
-	
+	/**
+	 * Obtém o atributo placa
+	 * @return Valor do atributo placa
+	 */
 	public String getPlaca() {
 		return placa;
 	}
-	
+	/**
+	 * Altera o atributo placa
+	 * @param placa Novo valor do atributo
+	 */
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
-	
+	/**
+	 * Obtém o atributo qtdPoltrona
+	 * @return Valor do atributo qtdPoltrona
+	 */
 	public Integer getQtdPoltrona() {
 		return qtdPoltrona;
 	}
-	
+	/**
+	 * Altera o atributo qtdPoltrona
+	 * @param qtdPoltrona Novo valor do atributo
+	 */
 	public void setQtdPoltrona(Integer qtdPoltrona) {
 		this.qtdPoltrona = qtdPoltrona;
 	}
+	
 }
