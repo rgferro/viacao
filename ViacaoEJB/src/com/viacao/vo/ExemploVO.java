befd8447ace1aabf4df7199a3a8aeb6f2337981b
wallacecosme <wallacecosme@gmail.com>
2011-06-13 18:32:04 +0000

    Wallace Gonçalves

--- ViacaoEJB/src/com/viacao/vo/ExemploVO.java
+++ ViacaoEJB/src/com/viacao/vo/ExemploVO.java
@@ -2,25 +2,49 @@ package com.viacao.vo;
 
 public class ExemploVO {
 	
-	Integer seqExemplo;
-	String nomeExemplo;
-	String descricao;
+	private Integer seqExemplo;
+	private String nomeExemplo;
+	private String descricao;
 	
+	/**
+	 * Obtém o atributo seqExemplo
+	 * @return Valor do atributo seqExemplo
+	 */
 	public Integer getSeqExemplo() {
 		return seqExemplo;
 	}
+	/**
+	 * Altera o atributo seqExemplo
+	 * @param seqExemplo Novo valor do atributo
+	 */
 	public void setSeqExemplo(Integer seqExemplo) {
 		this.seqExemplo = seqExemplo;
 	}
+	/**
+	 * Obtém o atributo nomeExemplo
+	 * @return Valor do atributo nomeExemplo
+	 */
 	public String getNomeExemplo() {
 		return nomeExemplo;
 	}
+	/**
+	 * Altera o atributo nomeExemplo
+	 * @param nomeExemplo Novo valor do atributo
+	 */
 	public void setNomeExemplo(String nomeExemplo) {
 		this.nomeExemplo = nomeExemplo;
 	}
+	/**
+	 * Obtém o atributo descricao
+	 * @return Valor do atributo descricao
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
+	/**
+	 * Altera o atributo descricao
+	 * @param descricao Novo valor do atributo
+	 */
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
