d33703292bba7e510cacd8b30066395660b0c2e1
wallacecosme@gmail.com <wallacecosme@gmail.com@f410aa4f-024f-7bfd-2858-2e2917c05c61>
2011-06-14 19:56:27 +0000

    Wallace Gonçalves - Codificação do UC - 004 Manteritinerario.

--- trunk/ViacaoEJB/src/com/viacao/interfaces/ManterCadastro.java
+++ trunk/ViacaoEJB/src/com/viacao/interfaces/ManterCadastro.java
@@ -3,37 +3,30 @@ package com.viacao.interfaces;
 import java.rmi.RemoteException;
 import java.util.List;
 
-import java.rmi.RemoteException;
-import java.util.List;
-
-import javax.ejb.EJBException;
 import javax.ejb.EJBObject;
 
-import com.viacao.vo.ExemploVO;
-import com.viacao.vo.TarifaVO;
-
-import com.acol.exception.DAOException;
-import com.viacao.dao.OnibusDAO;
+import com.viacao.vo.ItinerarioVo;
 import com.viacao.vo.OnibusVO;
+import com.viacao.vo.TarifaVO;
 
 public interface ManterCadastro extends EJBObject {
 	
-	public void inserir(TarifaVO tarifaVO) throws RemoteException;
-	
-	public void deletar(TarifaVO tarifaVO) throws RemoteException;
-	
-	public void alterar(TarifaVO tarifaVO) throws RemoteException;
-	
-	public List getListaTarifa(TarifaVO tarifaVO) throws RemoteException;
-	
+	public void inserir(TarifaVO tarifaVO) throws RemoteException;	
+	public void deletar(TarifaVO tarifaVO) throws RemoteException;	
+	public void alterar(TarifaVO tarifaVO) throws RemoteException;	
+	public List<TarifaVO> getListaTarifa(TarifaVO tarifaVO) throws RemoteException;	
 	public TarifaVO getTarifa(TarifaVO tarifaVO) throws RemoteException;
 	
-	public void inserir(OnibusVO onibusVO) throws RemoteException;
+	public void inserir(OnibusVO onibusVO) throws RemoteException;	
+	public void deletar(OnibusVO onibusVO) throws RemoteException;	
+	public void alterar(OnibusVO onibusVO) throws RemoteException;	
+	public List<OnibusVO> getListaOnibus(OnibusVO onibusVO) throws RemoteException;	
+	public OnibusVO getOnibus(OnibusVO onibusVO) throws RemoteException;	
 	
-	public void deletar(OnibusVO onibusVO) throws RemoteException;
-	
-	public void alterar(OnibusVO onibusVO) throws RemoteException;
-	
-	public List getListaOnibus(OnibusVO onibusVO) throws RemoteException;
+	public void inserirItinerario (ItinerarioVo itinerarioVo) throws RemoteException;
+	public void deletarItinerario(ItinerarioVo itinerarioVo) throws RemoteException;	
+	public void alterarItinerario(ItinerarioVo itinerarioVo) throws RemoteException;
+	public ItinerarioVo getItinerario(ItinerarioVo itinerarioVo) throws RemoteException;
+	public List<ItinerarioVo> listaItinerario(ItinerarioVo itinerarioVo) throws RemoteException;		
 
 }
