2b21224856f45bee0ea3ed67cfd52172bb30a375
wallacecosme@gmail.com <wallacecosme@gmail.com@f410aa4f-024f-7bfd-2858-2e2917c05c61>
2011-06-14 19:37:43 +0000

    Wallace Gonçalves - Codificação do UC - 004 Manteritinerario.

--- trunk/ViacaoEJB/src/com/viacao/vo/EnderecoVO.java
+++ trunk/ViacaoEJB/src/com/viacao/vo/EnderecoVO.java
@@ -9,47 +9,103 @@ public class EnderecoVO {
 	private String cidade;
 	private String estado;
 	
+	
+	/**
+	 * Obtém o atributo seqEndereco
+	 * @return Valor do atributo seqEndereco
+	 */
 	public Integer getSeqEndereco() {
 		return seqEndereco;
 	}
+	/**
+	 * Altera o atributo seqEndereco
+	 * @param seqEndereco Novo valor do atributo
+	 */
 	public void setSeqEndereco(Integer seqEndereco) {
 		this.seqEndereco = seqEndereco;
 	}
+	/**
+	 * Obtém o atributo logradouro
+	 * @return Valor do atributo logradouro
+	 */
 	public String getLogradouro() {
 		return logradouro;
 	}
+	/**
+	 * Altera o atributo logradouro
+	 * @param logradouro Novo valor do atributo
+	 */
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
+	/**
+	 * Obtém o atributo numero
+	 * @return Valor do atributo numero
+	 */
 	public String getNumero() {
 		return numero;
 	}
+	/**
+	 * Altera o atributo numero
+	 * @param numero Novo valor do atributo
+	 */
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+	/**
+	 * Obtém o atributo complemento
+	 * @return Valor do atributo complemento
+	 */
 	public String getComplemento() {
 		return complemento;
 	}
+	/**
+	 * Altera o atributo complemento
+	 * @param complemento Novo valor do atributo
+	 */
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
+	/**
+	 * Obtém o atributo bairro
+	 * @return Valor do atributo bairro
+	 */
 	public String getBairro() {
 		return bairro;
 	}
+	/**
+	 * Altera o atributo bairro
+	 * @param bairro Novo valor do atributo
+	 */
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+	/**
+	 * Obtém o atributo cidade
+	 * @return Valor do atributo cidade
+	 */
 	public String getCidade() {
 		return cidade;
 	}
+	/**
+	 * Altera o atributo cidade
+	 * @param cidade Novo valor do atributo
+	 */
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+	/**
+	 * Obtém o atributo estado
+	 * @return Valor do atributo estado
+	 */
 	public String getEstado() {
 		return estado;
 	}
+	/**
+	 * Altera o atributo estado
+	 * @param estado Novo valor do atributo
+	 */
 	public void setEstado(String estado) {
 		this.estado = estado;
-	}
-	
+	}	
 }
