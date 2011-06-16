8f995e315e040bd049c9083d90d1f9c591ed471a
wallacecosme@gmail.com <wallacecosme@gmail.com@f410aa4f-024f-7bfd-2858-2e2917c05c61>
2011-06-16 15:01:04 +0000

    Wallace Gonçalves - Codificação do UC - 004 Manteritinerario.

--- trunk/ViacaoWeb/src/com/viacao/struts/form/ManterItinerarioForm.java
+++ trunk/ViacaoWeb/src/com/viacao/struts/form/ManterItinerarioForm.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.apache.struts.action.ActionForm;
 
 import com.viacao.vo.ItinerarioVo;
+import com.viacao.vo.TarifaVO;
 
 /**
  * @author Wallace Gonçalves
@@ -19,11 +20,22 @@ public class ManterItinerarioForm extends ActionForm{
 	private static final long serialVersionUID = 1L;
 	
 	private ItinerarioVo itinerarioVo;
-	private List<ItinerarioVo> lista;
+	private List<TarifaVO> listaTarifas;
+	private List<TarifaVO> listaTarifasEscolhidas;
+	private List<ItinerarioVo> listaItinerario;
+	private String seq;
+	private String cboTarifa;
+	private String task;
+	private boolean isconfirme;
 	
 	public void inicializar(){
 		this.itinerarioVo = new ItinerarioVo();
-		this.lista = new ArrayList<ItinerarioVo>();
+		this.listaItinerario = new ArrayList<ItinerarioVo>();
+		this.listaTarifas = new ArrayList<TarifaVO>();
+		this.listaTarifasEscolhidas = new ArrayList<TarifaVO>();
+		this.seq = "1";
+		this.cboTarifa = "";
+		this.isconfirme = false;
 	}
 
 	/**
@@ -41,20 +53,115 @@ public class ManterItinerarioForm extends ActionForm{
 	public void setItinerarioVo(ItinerarioVo itinerarioVo) {
 		this.itinerarioVo = itinerarioVo;
 	}
-
 	/**
-	 * Obtém o atributo lista
-	 * @return Valor do atributo lista
+	 * Obtém o atributo seq
+	 * @return Valor do atributo seq
 	 */
-	public List<ItinerarioVo> getLista() {
-		return lista;
+	public String getSeq() {
+		return seq;
 	}
 
 	/**
-	 * Altera o atributo lista
-	 * @param lista Novo valor do atributo
+	 * Altera o atributo seq
+	 * @param seq Novo valor do atributo
 	 */
-	public void setLista(List<ItinerarioVo> lista) {
-		this.lista = lista;
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+
+	/**
+	 * Obtém o atributo listaTarifas
+	 * @return Valor do atributo listaTarifas
+	 */
+	public List<TarifaVO> getListaTarifas() {
+		return listaTarifas;
+	}
+
+	/**
+	 * Altera o atributo listaTarifas
+	 * @param listaTarifas Novo valor do atributo
+	 */
+	public void setListaTarifas(List<TarifaVO> listaTarifas) {
+		this.listaTarifas = listaTarifas;
+	}
+
+	/**
+	 * Obtém o atributo listaTarifasEscolhidas
+	 * @return Valor do atributo listaTarifasEscolhidas
+	 */
+	public List<TarifaVO> getListaTarifasEscolhidas() {
+		return listaTarifasEscolhidas;
+	}
+
+	/**
+	 * Altera o atributo listaTarifasEscolhidas
+	 * @param listaTarifasEscolhidas Novo valor do atributo
+	 */
+	public void setListaTarifasEscolhidas(List<TarifaVO> listaTarifasEscolhidas) {
+		this.listaTarifasEscolhidas = listaTarifasEscolhidas;
+	}
+
+	/**
+	 * Obtém o atributo task
+	 * @return Valor do atributo task
+	 */
+	public String getTask() {
+		return task;
+	}
+
+	/**
+	 * Altera o atributo task
+	 * @param task Novo valor do atributo
+	 */
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	/**
+	 * Obtém o atributo listaItinerario
+	 * @return Valor do atributo listaItinerario
+	 */
+	public List<ItinerarioVo> getListaItinerario() {
+		return listaItinerario;
+	}
+
+	/**
+	 * Altera o atributo listaItinerario
+	 * @param listaItinerario Novo valor do atributo
+	 */
+	public void setListaItinerario(List<ItinerarioVo> listaItinerario) {
+		this.listaItinerario = listaItinerario;
+	}
+
+	/**
+	 * Obtém o atributo isconfirme
+	 * @return Valor do atributo isconfirme
+	 */
+	public boolean isIsconfirme() {
+		return isconfirme;
+	}
+
+	/**
+	 * Altera o atributo isconfirme
+	 * @param isconfirme Novo valor do atributo
+	 */
+	public void setIsconfirme(boolean isconfirme) {
+		this.isconfirme = isconfirme;
+	}
+
+	/**
+	 * Obtém o atributo cboTarifa
+	 * @return Valor do atributo cboTarifa
+	 */
+	public String getCboTarifa() {
+		return cboTarifa;
+	}
+
+	/**
+	 * Altera o atributo cboTarifa
+	 * @param cboTarifa Novo valor do atributo
+	 */
+	public void setCboTarifa(String cboTarifa) {
+		this.cboTarifa = cboTarifa;
 	}
 }
